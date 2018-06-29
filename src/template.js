bf9ce0e8875f2274178124a91a46cd987df7d228
Marcus Armstrong <marcusdarmstrong@gmail.com>
2018-06-28 20:49:38 -0400

    Serving HTML

---
+++ src/template.js
@@ -0,0 +1,2 @@
+export default ({ title, body }) => 
+  `<!doctype html><html lang="en"><head><meta charset="utf-8"><title>${title}</title><script src="static/client.js" async></script></head><body>${body}</body></html>`;
